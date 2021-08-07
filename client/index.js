b7f436e4192bff9aa90d618ce10b525584730d80
mehdi-bh <benharizmehdi20@gmail.com>
2021-08-07 09:57:00 +0100

    finished the front

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
 const inpt1 = document.querySelector("#url");
-const inpt2 = document.querySelector("#search");
+const inpt2 = document.querySelector("#search-inpt");
+const inpt3 = document.querySelector("#custom");
 const btn1 = document.querySelector("#submit");
-const btn2 = document.querySelector("#search");
+const btn2 = document.querySelector("#search-btn");
 const result = document.querySelector("#result");
 
 console.log("index.js loaded");
 console.log(inpt1.innerHTML);
 
-const getURL = async () => {
+const getURL = async (longURL, custom) => {
   const res = await fetch("http://localhost:3000/generate-url", {
     method: "POST",
     headers: {
@@ -15,33 +16,34 @@ const getURL = async () => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      longURL: "https://www.youtube.com/watch?v=9gCLyBlEGOw",
-      custom: "one",
+      longURL,
+      custom,
     }),
   });
   const result = await res.json();
   return result;
 };
 
-const search = async () => {
-  const res = await fetch("http://localhost:3000/", {
+const search = async (ID) => {
+  const res = await fetch("http://localhost:3000/" + ID, {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      longURL: "https://www.youtube.com/watch?v=9gCLyBlEGOw",
-      custom: "one",
-    }),
   });
-  const result = await res.json();
-  return result;
 };
 
 btn1.addEventListener("click", async () => {
-  const shortURL = await getURL();
+  const longURL = inpt1.value;
+  const custom = inpt3.value;
+  const shortURL = await getURL(longURL, custom);
   console.log(shortURL);
   result.innerHTML = shortURL;
 });
 
-btn2.addEventListener("click", async () => {});
+btn2.addEventListener("click", async () => {
+  console.log("searching");
+  const ID = inpt2.value.substring(22);
+  console.log(ID);
+  search(ID);
+});
